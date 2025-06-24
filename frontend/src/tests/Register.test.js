459373ee7a3d5345f4a17ad78bd386470c722d2f
Dor <dormitnik16@gmail.com>
2025-06-24 17:38:50 +0300

    removed front tests

--- frontend/src/tests/Register.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Register from '../components/Register';
-
-test('renders register form', () => {
-  render(<Register />);
-  expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
-});
